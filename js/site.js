c2229f27f0bb06bd0c02f6685fbf1bbeca1fb9ef
malcolmlovell19 <mlovel01@nyit.edu>
2021-11-03 14:34:03 -0400

    Initial Commit
    App logic is completed, need to add custom logo and links to social media/portfolio

--- js/site.js
+++ js/site.js
@@ -1,4 +1,29 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World!");
+//Get string from page
+function getValue(){
+    //Make sure alert is invisible before button is pressed
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+function reverseString(userString){
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+      revString += userString[i];         
+    }
+    
+    return revString;
+}
+
+//Display the reversed string to the user
+function displayString(revString){
+    document.getElementById("msg").innerHTML = `Your reversed string is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");        
 }
